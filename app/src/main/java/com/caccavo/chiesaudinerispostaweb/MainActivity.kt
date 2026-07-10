3c1f901ca73d04b102c03083d79f90185d5ca844
salvocac <caccavo.s@gmail.com>
2026-07-10 16:25:59 +0000

    Rimuovi la splash screen col logo

--- app/src/main/java/com/caccavo/chiesaudinerispostaweb/MainActivity.kt
+++ app/src/main/java/com/caccavo/chiesaudinerispostaweb/MainActivity.kt
@@ -3,7 +3,6 @@ package com.caccavo.chiesaudinerispostaweb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -25,7 +24,6 @@ private const val ROUTE_USER_GUIDE = "userGuide"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             ChiesaTheme {
